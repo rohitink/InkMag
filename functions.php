96bb67234abf7c9c17e1bcf6352338aa4ba2a21e
Rohit Tripathi <rohitink@live.com>
2013-07-05 13:43:43 +0530

    Version 1.1
    More details in change_log.txt

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function inkmag_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
@@ -53,6 +53,7 @@ function inkmag_setup() {
 	 * Enable support for Post Formats
 	 */
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	
 }
 endif; // inkmag_setup
 add_action( 'after_setup_theme', 'inkmag_setup' );
@@ -147,5 +148,3 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
-
-add_theme_support( 'post-thumbnails' );
